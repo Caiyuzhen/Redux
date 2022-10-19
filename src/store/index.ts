5d54dc74fd1b991c9ccdc22eea2fd4d1d61c0e43
yuzhen <caiyuzhen00@gmail.com>
2022-10-19 14:13:23 +0800

    fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,7 @@ const store = createStore(reducers, composeWithDevTools(applyMiddleware())) //�
 
 
 //👇导出根 state 的【 rootState 类型】(ReturnType 表示这个对象是什么类型，就返回什么类型), 用来规范【要映射到组件内的数据】的类型
-//⚠️注意，这个类型会在 【业务组件】 调用 {useSelector} 内进行使用, 比如 const admin = useSelector((state: rootState))
+//⚠️注意，这个类型会在 【业务组件】 调用 {useSelector} 内进行使用, 比如 const admin = useSelector((state: rootState)=>admin )
 export type rootState = ReturnType<typeof reducers>
 
 export default store
